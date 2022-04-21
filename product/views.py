0c9ab4304e5483bbbf57e26be006710b8b56c3d5
Smart <smartlife1257@gmail.com>
2022-04-20 18:09:37 -0700

    Included Templates
    Included html, css, javascript, font-awesome. Although ignored font-awesome files cause it has a large file

--- product/views.py
+++ product/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product, Category
 
 # Create your views here.
+
+class HomePageView(ListView):
+    model = Product
+    template_name = 'product/index.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["category"] = Category.objects.all()[:4]
+        return context
+    
